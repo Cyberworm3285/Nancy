3eb00ffd0c3bd7da8d8f7be69c1a59d67fdab3d2
cyberworm3285 <cyberworm3285@web.de>
2018-02-11 01:47:27 +0100

    ohne locks

---
+++ DepenNancy/Injector.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Nancy
+{
+    public static class Injector
+    {
+        private static ConcurrentDictionary<Type, object> _singeltons = new ConcurrentDictionary<Type, object>();
+        private static ConcurrentDictionary<Type, Lazy<object>> _ctors = new ConcurrentDictionary<Type, Lazy<object>>();
+
+        public static void RegisterType<T>(Func<object> pseudoCtor)
+        {
+            Type t = typeof(T);
+            if (_ctors.ContainsKey(t))
+                throw new TypeAccessException("type already registered");
+
+            _ctors.GetOrAdd(t, new Lazy<object>(pseudoCtor));
+        }
+
+        public static void RegisterType<T>()
+            where T : new()
+        {
+            RegisterType<T>(() => new T());
+        }
+
+        public static T ResolveType<T>()
+        {
+            var t = typeof(T);
+            if (!_ctors.ContainsKey(t))
+                throw new TypeAccessException($"no ctor for type {t.Name} registered");
+            if (!_ctors[t].IsValueCreated)
+                _singeltons.GetOrAdd(t, _ctors[t].Value);
+
+            return (T)_singeltons[t];
+        }
+
+        public static async Task<T> ResolveTypeAsync<T>()
+        {
+            var t = typeof(T);
+            if (!_ctors.ContainsKey(t))
+                throw new TypeAccessException($"no ctor for type {t.Name} registered");
+            if (!_ctors[t].IsValueCreated)
+            {
+                var task = new Task<T>(() => (T)_singeltons.GetOrAdd(t, _ctors[t].Value));
+                task.Start();
+                return await task;
+            }
+
+            return (T)_singeltons[t];
+        }
+
+        public static async Task<T> RegisterAndResolveAsync<T>(Func<object> pseudoCtor)
+        {
+            RegisterType<T>(pseudoCtor);
+            return await ResolveTypeAsync<T>();
+        }
+
+        public static async Task<T> RegisterAndResolveAsync<T>()
+            where T : new()
+        {
+            RegisterType<T>();
+            return await ResolveTypeAsync<T>();
+        }
+
+        public static T RegisterAndResolve<T>(Func<object> pseudoCtor)
+        {
+            RegisterType<T>(pseudoCtor);
+            return  ResolveType<T>();
+        }
+
+        public static T RegisterAndResolve<T>()
+            where T : new()
+        {
+            RegisterType<T>();
+            return ResolveType<T>();
+        }
+
+        public static async Task<bool> NullifyAsync<T>(Func<bool> cancelTrigger)
+        {
+            var t = new Task<bool>(() =>
+            {
+                while (!cancelTrigger())
+                {
+                    if (_singeltons.TryRemove(typeof(T), out object dummy))
+                        return true;
+                }
+                return false;
+            });
+            t.Start();
+            return await t;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+
+using static Nancy.Injector;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DewIt().GetAwaiter().GetResult();
+        }
+
+        static async Task DewIt()
+        {
+            Stopwatch watch = new Stopwatch();
+
+            Console.WriteLine("Registering Ctor..");
+
+            watch.Start();
+            RegisterType<A>(() => new A(1));
+            watch.Stop();
+            Console.WriteLine($" ({watch.ElapsedMilliseconds}ms)");
+            watch.Reset();
+
+            var task = ResolveTypeAsync<A>();
+            var nullBoi = NullifyAsync<A>(() => false);
+            Console.WriteLine("doing other stuff..(~1000ms)");
+            Thread.Sleep(1000);
+            Console.WriteLine("done sleeping");
+
+            Console.WriteLine("Resolving ..");
+
+            object a;
+
+            watch.Reset();
+            //if (await nullBoi)
+            //    Console.WriteLine("deleted shizz");
+            watch.Start();
+            a = await task;
+            watch.Stop();
+            Console.Write($"({watch.ElapsedMilliseconds}ms)");
+
+            Console.ReadKey();
+        }
+    }
+
+    class A
+    {
+        public int Value { get; set; }
+        public A(int v)
+        {
+            Console.WriteLine($">>[Ctor called (5000ms)] ");
+            Value = v;
+            Thread.Sleep(5000);
+            Console.WriteLine($">>[Ctor finished] ");
+        }
+        public override string ToString() => $"[{Value}]";
+    }
+}
